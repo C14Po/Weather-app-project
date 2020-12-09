cd9e64d31f489c412b3adf5a5c4b87f258ad1f83
C14Po <75749990+C14Po@users.noreply.github.com>
2020-12-09 19:02:36 +0100

    Move HTML, CSS and JS files into folder

---
+++ src/script.js
@@ -0,0 +1,76 @@
+function showTemp(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp);
+  
+  document.querySelector("#hum").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#weather").innerHTML =
+    response.data.weather[0].main;
+  
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+let apiKey = "e79392cca3f3cb28e460855640711538";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemp);
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+let city = document.querySelector("#search-input").value;
+searchCity(city);
+
+}
+
+function showPosition(position) {
+  let apiKey = "e79392cca3f3cb28e460855640711538";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+navigator.geolocation.getCurrentPosition(showPosition); }
+
+//I don't know how to get the city displayed. It says Undefined
+
+let currentInput = document.querySelector("button");
+currentInput.addEventListener("click", getCurrentPosition);
+
+let searchInput = document.querySelector("#form-groups");
+searchInput.addEventListener("submit", handleSubmit);
+
+
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+];
+
+document.querySelector("#date");
+let day = days[now.getDay()];
+let hour = (now.getHours() < 10 ? "0" : "") + now.getHours();
+let minute = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
+
+date.innerHTML = `${day} ${hour}:${minute}`;
+
+searchCity("London");
+
+
+
+
+
+
+
+
